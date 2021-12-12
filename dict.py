ce5a35bb079d818a96c90d59fd2c7ae26aa3ae1b
SURAJ SAFI <94732511+suraj-safi1@users.noreply.github.com>
2021-12-12 12:22:11 +0530

    Dictionary program

---
+++ dict.py
@@ -0,0 +1,13 @@
+#dictionay program
+mydict={
+        "fast":"horse is fast",
+        "suraj":"hero",
+        "marks":[10,90,89,76,88],
+        "anotherdict": {"suraj": "scientiest"}
+        }
+print(mydict['suraj'])
+print(mydict['fast'])
+print(mydict['marks'])
+print(mydict['anotherdict']['suraj'])
+mydict['marks']=[23,45,61,23,45,78]
+print(mydict['marks'])
